4683ab087803052289d5e4fa59c24112adc76a1d
Jonathan Raposo <jonathan.raposo.luna@gmail.com>
2025-07-23 04:35:24 -0400

    modify

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,24 @@ import bodyParser from './middlewares/bodyParser';
 const app = express();
 
 app.use(bodyParser())
+
+
+/*
+REST endpoints provide:
+
+POST /api/users/create — create user
+
+GET /api/users — list all users
+
+GET /api/users/:id — get user by ID
+
+GET /api/users/search?q=keyword — search users
+
+POST /api/users/:id/update — update user
+
+POST /api/users/:id/delete — delete user 
+ */ 
+
 app.use('/',usersRouter )
 
 const PORT = process.env.PORT || 3000;
